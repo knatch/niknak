ef6fda98db97d141ac4b28686d9a2e9337a38667
Natch Khongpasuk <natch.kh@gmail.com>
2019-12-03 22:30:06 -0500

    deducted points on creating post with offensive words

--- app/src/main/java/com/example/nik_nak/NewPostActivity.kt
+++ app/src/main/java/com/example/nik_nak/NewPostActivity.kt
@@ -1,6 +1,7 @@
 package com.example.nik_nak
 
 import android.content.Context
+import android.content.DialogInterface
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,8 @@ import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import android.util.Log
+import android.widget.Toolbar
+import androidx.appcompat.app.AlertDialog
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.Timestamp
@@ -20,6 +23,9 @@ import com.google.firebase.firestore.GeoPoint
 class NewPostActivity : AppCompatActivity() {
 
     private val TAG = "New Post"
+
+    private val db = FirebaseFirestore.getInstance()
+
     private val words = listOf("bitch", "nigger", "nigga", "asshole", "fuck", "motherfucker", "doochbag", "idiot", "dick", "gay", "faggot", "handjob", "f**k", "bastard")
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -83,8 +89,9 @@ class NewPostActivity : AppCompatActivity() {
                 }
 
                 if (!isClean) {
-                    val myToast = Toast.makeText(this, "You are posting inappropriate message.", Toast.LENGTH_SHORT)
-                    myToast.show()
+                    proceedDialog()
+                    // val myToast = Toast.makeText(this, "You are posting inappropriate message.", Toast.LENGTH_SHORT)
+                    // myToast.show()
                 } else {
                     // display posting status
                     val myToast = Toast.makeText(this, "Sending...", Toast.LENGTH_SHORT)
@@ -92,8 +99,7 @@ class NewPostActivity : AppCompatActivity() {
 
                     val currentTime = Timestamp.now()
 
-                    val sharedPref =
-                        getSharedPreferences(getString(R.string.user_id_key), Context.MODE_PRIVATE)
+                    val sharedPref = getSharedPreferences(getString(R.string.user_id_key), Context.MODE_PRIVATE)
                     val userId = sharedPref.getString(getString(R.string.user_id_key), "")
 
                     fusedLocationClient.lastLocation
@@ -108,50 +114,39 @@ class NewPostActivity : AppCompatActivity() {
                                 "points" to 0,
                                 "userId" to userId
                             )
-                            // Access a Cloud Firestore instance from your Activity
-                            val db = FirebaseFirestore.getInstance()
 
                             db.collection("posts")
                                 .add(newPost)
                                 .addOnSuccessListener { documentReference ->
-                                    Log.d(
-                                        TAG,
-                                        "DocumentSnapshot written with ID: ${documentReference.id}"
-                                    )
+                                    Log.d(TAG, "DocumentSnapshot written with ID: ${documentReference.id}")
 
                                     if (userId != null) {
 
                                         val userRef = db.collection("users").document(userId)
-                                        val sharedPref = getSharedPreferences(
-                                            getString(R.string.user_points_key),
-                                            Context.MODE_PRIVATE
-                                        )
-                                        val userPoints = sharedPref.getString(
-                                            getString(R.string.user_points_key),
-                                            "0"
-                                        )
 
+                                        val sharedPref = getSharedPreferences(getString(R.string.user_points_key), Context.MODE_PRIVATE)
+                                        val userPoints = sharedPref.getString(getString(R.string.user_points_key),"0")
                                         var points = userPoints.toString().toInt()
+                                        points += 5
 
-                                        if (userPoints!!.isEmpty()) {
-                                            points = 150
-                                        } else {
-                                            points += 5
-                                        }
                                         userRef.update("points", points)
                                             .addOnSuccessListener {
                                                 Log.d(TAG, "User points successfully updated!")
+
+                                                val editPref = sharedPref.edit()
+                                                editPref.putString(getString(R.string.user_points_key), points.toString())
+                                                editPref.apply()
+
+                                                // redirect back to main page
+                                                Handler().postDelayed({
+                                                    myToast.cancel()
+                                                    finish()
+                                                }, 1000)
                                             }
                                             .addOnFailureListener { e ->
                                                 Log.w(TAG, "Error updating user points", e)
                                             }
                                     }
-
-                                    // redirect back to main page
-                                    Handler().postDelayed({
-                                        myToast.cancel()
-                                        finish()
-                                    }, 1000)
                                 }
                                 .addOnFailureListener { e ->
                                     Log.w(TAG, "Error adding document", e)
@@ -169,5 +164,59 @@ class NewPostActivity : AppCompatActivity() {
             // Invoke the superclass to handle it.
             super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun proceedDialog () {
+        val dialogBuilder = AlertDialog.Builder(this)
+        // set message of alert dialog
+        dialogBuilder.setMessage("Your post contains offensive language. Your Yakarma point will be deducted. Are you sure you want to proceed?")
+            // if the dialog is cancelable
+            .setCancelable(false)
+            // positive button text and action
+            .setPositiveButton("Proceed", DialogInterface.OnClickListener {
+                    dialog, id -> updatePoints()
+            })
+            // negative button text and action
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+
+        // create dialog box
+        val alert = dialogBuilder.create()
+        // set title for alert dialog box
+        alert.setTitle("Warning")
+        // show alert dialog
+        alert.show()
+    }
+
+    private fun updatePoints () {
+
+        val sharedPref = getSharedPreferences(getString(R.string.user_id_key), Context.MODE_PRIVATE)
+        val userId = sharedPref.getString(getString(R.string.user_id_key), "")
+
+        val pointSharedPref = getSharedPreferences(getString(R.string.user_points_key), Context.MODE_PRIVATE)
+        val userPoints = pointSharedPref.getString(getString(R.string.user_points_key),"0")
+        var points = userPoints.toString().toInt()
+        Log.d(TAG, "updatePoints $points")
+        if (points >= 10) {
+            points -= 10
+        } else {
+            points = 0
+        }
+        Log.d(TAG, "updatePoints $points")
+        val userRef = db.collection("users").document(userId.toString())
+        userRef.update("points", points)
+            .addOnSuccessListener {
+                Log.d(TAG, "DocumentSnapshot successfully written!")
+                val editPref = sharedPref.edit()
+                editPref.putString(getString(R.string.user_points_key), points.toString())
+                editPref.apply()
+
+                // redirect back to main page
+                Handler().postDelayed({
+                    finish()
+                }, 1000)
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
     }
 }
